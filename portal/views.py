3de474c0342ba8a9b4f6de8fb0ff95887b4b02f0
Afsalsyed <afsalsyed12@gmail.com>
2024-07-06 11:40:22 +0530

    db connected

--- portal/views.py
+++ portal/views.py
@@ -6,28 +6,35 @@ from django.db import transaction # type: ignore
 
 def alumni_form_view(request):
     if request.method == 'POST':
-        alumni_form = AlumniForm(request.POST, request.FILES, prefix='alumni')
-        questionnaire_form = QuestionnaireForm(request.POST, prefix='questionnaire')
+        alumni_form = AlumniForm(request.POST, request.FILES)
+        #print(alumni_form,'check')
+        questionnaire_form = QuestionnaireForm(request.POST)
 
         try:
             with transaction.atomic():
-                if alumni_form.is_valid() and questionnaire_form.is_valid():
+                if alumni_form.is_valid():
+                    # if alumni_form.cleaned_data['educational_qualification'] == 'O':
+                    #     alumni_form.educational_qualification = alumni_form.cleaned_data['educational_qualification_other']
                     alumni = alumni_form.save(commit=False)
-                    if alumni_form.cleaned_data['educational_qualification'] == 'O':
-                        alumni.educational_qualification = alumni_form.cleaned_data['educational_qualification_other']
-                    alumni.save()
 
+                    alumni.save()
+                    #print("Alumni data saved successfully:", alumni_form)
+                    #return JsonResponse({'success': True})
+                else:
+                    errors = {
+                        'alumni_errors': alumni_form.errors,
+                    }
+                    print("Form errors:", errors)
+                if questionnaire_form.is_valid():
                     questionnaire = questionnaire_form.save(commit=False)
                     questionnaire.alumni = alumni
                     questionnaire.save()
 
-                    print("Alumni data saved successfully:", alumni)
                     print("Questionnaire data saved successfully:", questionnaire)
 
                     return JsonResponse({'success': True})
                 else:
                     errors = {
-                        'alumni_errors': alumni_form.errors,
                         'questionnaire_errors': questionnaire_form.errors
                     }
                     print("Form errors:", errors)
@@ -38,8 +45,8 @@ def alumni_form_view(request):
             return JsonResponse({'success': False, 'errors': str(e)})
 
     else:
-        alumni_form = AlumniForm(prefix='alumni')
-        questionnaire_form = QuestionnaireForm(prefix='questionnaire')
+        alumni_form = AlumniForm()
+        questionnaire_form = QuestionnaireForm()
 
     context = {
         'alumni_form': alumni_form,
