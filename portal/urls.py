29d7f8ca0679f766deef7a640ce4c7771468386e
Afsalsyed <afsalsyed12@gmail.com>
2024-07-07 23:49:16 +0530

    submit success

--- portal/urls.py
+++ portal/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('alumni/', views.alumni_create_view, name='alumni_create'),
-    path('alumni/success/', views.alumni_success_view, name='alumni_success'),
+    #path('alumni/success/', views.alumni_success_view, name='alumni_success'),
 ]
--- portal/views.py
+++ portal/views.py
@@ -2,24 +2,24 @@
 from django.shortcuts import render, redirect
 from .forms import AlumniForm, QuestionnaireForm
 from .models import Alumni, Questionnaire
+from django.contrib import messages
 
 def alumni_create_view(request):
     if request.method == 'POST':
         alumni_form = AlumniForm(request.POST, request.FILES)
-        #print(alumni_form)
         questionnaire_form = QuestionnaireForm(request.POST)
-        #print(questionnaire_form)
         if alumni_form.is_valid() and questionnaire_form.is_valid():
             alumni = alumni_form.save()
             alumni.save()
             questionnaire = questionnaire_form.save(commit=False)
             questionnaire.alumni = alumni
             questionnaire.save()
-            return redirect('alumni_success')
+            messages.success(request, 'Profile added successfully.')
+            return redirect('alumni_create')
+        else:
+            messages.error(request, 'There was an error with your submission. Please correct the errors below.')
     else:
         alumni_form = AlumniForm()
         questionnaire_form = QuestionnaireForm()
     return render(request, 'portal/alumni_form.html', {'alumni_form': alumni_form, 'questionnaire_form': questionnaire_form})
 
-def alumni_success_view(request):
-    return render(request, 'portal/alumni_success.html')
